38f1adf2d606ff58248c13e8ed4e208f787f831c
Eric Ho <ericho@101medialabs-imac-ho-ho.local>
2016-04-15 17:37:08 +0800

    Update alpha parameter

--- EHSharecode/UIImage+Extension.swift
+++ EHSharecode/UIImage+Extension.swift
@@ -22,7 +22,7 @@ public extension UIColor {
         let r = CGFloat(arc4random() % 255) / 255.0
         let g = CGFloat(arc4random() % 255) / 255.0
         let b = CGFloat(arc4random() % 255) / 255.0
-        return UIColor(red: r, green: g, blue: b, alpha: 0.5)
+        return UIColor(red: r, green: g, blue: b, alpha: 0.8)
     }
 }
 
